8c49fcebb2d613eee489d14d1bb00cdc60e15ce2
Pranit Rane <pranitrane619@gmail.com>
2021-09-30 04:52:52 +0530

    update: ui changes [IV]
    1. dialog on completing the game
    2. switch for toggling tile number on and off
    3. game fragment ui refined
    4. tile id on top of tile image
    5. content description for reset button

--- app/src/main/java/com/example/tilesmatch/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/tilesmatch/ui/game/GameFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -17,7 +18,9 @@ import com.example.tilesmatch.models.Resource
 import com.example.tilesmatch.models.Status
 import com.example.tilesmatch.models.Tile
 import com.example.tilesmatch.utils.MoveHelperUtils
+import com.example.tilesmatch.utils.extensions.buildConfirmationDialog
 import com.example.tilesmatch.utils.extensions.glide
+import com.example.tilesmatch.utils.extensions.showShortSnackBar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlin.properties.Delegates
 
@@ -48,13 +51,17 @@ class GameFragment : BaseFragment() {
             container, false
         )
 
+        val option = args.option
+
+        binding.txtGameTitle.text = option.title
+
         binding.txtMovesCounter.text = "Moves: $count"
 
         setRecyclerview()
 
         setListeners()
 
-        if (viewModel.tiles.value?.data.isNullOrEmpty()) viewModel.getGameTiles(glide, args.option)
+        if (viewModel.tiles.value?.data.isNullOrEmpty()) viewModel.getGameTiles(glide, option)
 
         viewModel.tiles
             .observe(viewLifecycleOwner, { resource -> handleTilesResource(resource) })
@@ -82,17 +89,23 @@ class GameFragment : BaseFragment() {
 
     private fun setListeners() {
         binding.imgBtnBack.setOnClickListener { this.onBackPressed() }
-        binding.btnReset.setOnClickListener {
-            count = 0
-            currentData.clear()
-            currentData.addAll(data)
-            adapter?.swapData(currentData)
+        binding.switchEnableIds.setOnCheckedChangeListener { _, isChecked ->
+            val list = currentData.map { it.copy(shouldShowId = isChecked) }
+            updateCurrentData(list)
         }
+        binding.imgBtnReset.setOnClickListener { resetGameTiles() }
     }
 
     private fun validateState() {
         if (isValid()) {
-            Log.d(TAG, "TestLog: You Won!")
+            val dialog = AlertDialog.Builder(requireContext())
+                .buildConfirmationDialog(
+                    inflater = layoutInflater,
+                    message = "Congratulations!\nYou Won!\nWanna go again?",
+                    positiveButtonClick = { resetGameTiles() },
+                    negativeButtonClick = { this.onBackPressed() }
+                )
+            dialog.show()
         }
     }
 
@@ -113,11 +126,22 @@ class GameFragment : BaseFragment() {
                 }
                 data.clear()
                 data.addAll(list)
-                currentData.clear()
-                currentData.addAll(list)
-                adapter?.swapData(currentData)
+                updateCurrentData(list)
             }
         }
+    }
+
+    private fun resetGameTiles() {
+        count = 0
+        updateCurrentData(data)
+        showShortSnackBar("Resetting...")
+    }
+
+    private fun updateCurrentData(list: List<Tile>) {
+        val mappedList = list.map { it.copy(shouldShowId = binding.switchEnableIds.isChecked) }
+        currentData.clear()
+        currentData.addAll(mappedList)
+        adapter?.swapData(currentData)
     }
 
     override fun onBackPressed() {
