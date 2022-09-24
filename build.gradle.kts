446675de581e1b2abe2bd1309292912f7608448b
Pranit Rane <pranitrane619@gmail.com>
2022-09-25 03:49:04 +0530

    upgrade: dependencies
    1. gradle
    2. kotlin
    3. navigation
    4. hilt
    5. android core
    6. app compat
    7. fragment
    8. coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
-        classpath(kotlin(module = "gradle-plugin", version = "1.6.10"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath(kotlin(module = "gradle-plugin", version = "1.7.10"))
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.2")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
     }
 }
 
