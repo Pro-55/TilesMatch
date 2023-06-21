c170788424f6310b077f0b5a8ed58591c04054ba
Pranit Rane <pranitrane619@gmail.com>
2023-06-21 23:57:53 +0530

    upgrade: dependencies
    1. gradle
    2. navigation
    4. kotlin
    5. core
    6. material
    7. fragment

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0")
+        classpath("com.android.tools.build:gradle:8.0.2")
         classpath(kotlin(module = "gradle-plugin", version = "1.8.0"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
     }
 }
