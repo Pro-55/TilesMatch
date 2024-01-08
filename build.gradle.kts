c40ada5b4cdf3c746adecf84d6c31477fae2918b
Pranit Rane <pranitrane619@gmail.com>
2024-01-08 14:11:27 +0530

    upgrade: dependencies
    1. gradle
    2. kotlin
    3. hilt
    4. navigation safeargs
    5. androidx core
    6. material
    7. fragment
    8. naviagtion
    9. recycler view
    10. glide
    11. coroutines
    12. gradle version management updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath(kotlin(module = "gradle-plugin", version = "1.8.0"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks {
-    val clean by registering(Delete::class) {
-        delete(buildDir)
-    }
+plugins {
+    alias(libs.plugins.android.gradle) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.navigation.safeargs) apply false
+    alias(libs.plugins.hilt) apply false
 }
